3162b2f315338779189631139a29547ad52f30c6
solomontang <solomoncztang@gmail.com>
2017-05-26 15:19:07 -0700

    sends a properly formatted message

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,26 @@
 // YOUR CODE HERE:
+//{ url: 'http://parse.hrsf.hackreactor.com/chatterbox/classes/messages' };
+var app = {
+  url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages'
+};
+
+app.init = function() {
+};
+
+app.send = function(message) {
+  $.ajax({
+    url: this.url,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function(data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function(data) {
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+
+//app.fetch
+
