efafa37a190ba18c200971d2a327ee229d036dfc
solomontang <solomoncztang@gmail.com>
2017-05-26 17:15:38 -0700

    can click usernames

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,10 +1,15 @@
 // YOUR CODE HERE:
 //{ url: 'http://parse.hrsf.hackreactor.com/chatterbox/classes/messages' };
+
+
+
 var app = {
   url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages'
 };
 
 app.init = function() {
+  // this.renderRoom();
+  $('.username').on('click', this.handleUsernameClick.call(this));
 };
 
 app.send = function(message) {
@@ -40,16 +45,40 @@ app.clearMessages = function() {
 };
 
 app.renderMessage = function(message) {
-  let textContent = message.text;
-  $('#chats').append(`<p>${textContent}</p>`);
+  let {username, text, roomname} = message;
+
+  let $messageWrapper = $('<div></div>');
+  $messageWrapper.attr('class', 'messageEl');
+
+  let $usernameEl = $(`<p>${username}</p>`);
+  $usernameEl.attr('class', 'username');
+
+  let $messageBodyEl = $(`<p>${text}</p>`);
+  $messageBodyEl.attr('class', 'messageBody');
+
+  $messageWrapper.append($usernameEl, $messageBodyEl);
+  $('#chats').append($messageWrapper);
+  //$('#chats').append(`<div><p class="${username}"></p><p><${text}</p></div>`);
 };
 
-app.renderRoom = function(roomName) {
+app.renderRoom = function(roomName = '') {
   //--------consider whether this replaces old selector-----
-  // if ($('#roomSelect')) {}
-  let $roomSelector = $('<select></select>');
-  $roomSelector.append(`<option>${roomName}</option>`);
-  $('#roomSelect').append($roomSelector);
+  var $roomSelector;
+  if ($('#roomSelect').length === 0) {
+    $roomSelector = $('<select></select>');
+    $roomSelector.attr('class', 'roomSelect');
+    $('#main').append($roomSelector);
+  }
+  $roomSelector = $('#roomSelect');
+  let $option = $('<option></option>');
+  $option.attr('value', roomName);
+  $option.text(roomName);
+  $roomSelector.append($option);
 };
 
+app.handleUsernameClick = function() {
+  console.log('clicked!');
 
+};
+
+// app.init();
